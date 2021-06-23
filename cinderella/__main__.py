90412a774870cd280a492003c730f1152900e39c
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2021-06-23 02:11:34 -0700

    Add files via upload

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -78,7 +78,7 @@ GDPR = []
 
 START_IMG = os.environ.get('START_IMG', None)
 if START_IMG is None:
-    img = "https://telegra.ph/file/511ad504656e712b88235.jpg"
+    img = "https://telegra.ph/file/7d64cbd248accefb41ebd.jpg"
 else:
   img = START_IMG    
     
@@ -179,8 +179,10 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="🤝Help",callback_data="help_back"),InlineKeyboardButton(text="🛡Creator🛡",url="https://t.me/Surv_ivor")]]
-    keyboard += [[InlineKeyboardButton(text="🌐Connect Group", callback_data="main_connect"),InlineKeyboardButton(text="⚜️Add Me⚜️",url="t.me/{}?startgroup=true".format(bot.username))]]
+    keyboard = [[InlineKeyboardButton(text="🤝Help",callback_data="help_back"),InlineKeyboardButton(text="Creator",url="https://t.me/Khiladiking45")]]
+    keyboard += [[InlineKeyboardButton(text="🌐Connect Group", callback_data="main_connect"),InlineKeyboardButton(text="⚜️Add Me⚜️",url="t.me/Anjalofgodbot?startgroup=true".format(bot.username))]]
+    keyboard += [[InlineKeyboardButton(text="support Group",url="https://t.me/GodOfAnjalsupport"),InlineKeyboardButton(text="Channel",url="https://t.me/Godofanjel_gban_logs")]]
+    keyboard += [[InlineKeyboardButton(text="Fedration",url="https://t.me/PiratesFed")]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
@@ -280,8 +282,8 @@ def get_help(bot: Bot, update: Update):
 
         update.effective_message.reply_text("Contact me in PM to get the list of possible commands.",
                                             reply_markup=InlineKeyboardMarkup(
-                                                [[InlineKeyboardButton(text="⚜️Help",url="t.me/{}?start=help".format(bot.username))],  
-                                                [InlineKeyboardButton(text="🛡Contact Creator",url="https://t.me/Surv_ivor")]]))
+                                                [[InlineKeyboardButton(text="⚜️Help",url="t.me/Anjalofgod?start=help".format(bot.username))],  
+                                                [InlineKeyboardButton(text="🛡Contact Creator",url="https://t.me/Khiladiking45")]]))
         return
 
     elif len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
